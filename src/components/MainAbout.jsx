5b1404221fd488f30215d271754a6f7c1bb55566
Etienne Trzpit <etienne.trzpit@free.fr>
2023-09-09 11:11:27 +0200

    wrapper transition

--- src/components/MainAbout.jsx
+++ src/components/MainAbout.jsx
@@ -1,4 +1,5 @@
 import "../assets/style/accordion.scss";
+import { useState } from "react";
 
 export default function MainAbout() {
   let accordionTitle = ["Fiabilité", "Respect", "Service", "Sécurité"];
@@ -8,15 +9,36 @@ export default function MainAbout() {
     "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
     "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bienà l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
   ];
+  const [selected, setSelected] = useState(null);
+  const toggle = (index) => {
+    if (selected === index) {
+      return setSelected(null);
+    }
+    setSelected(index);
+  };
+
   return (
     <div className="accordions">
       {accordionTitle.map((title, index) => (
-        <div className="accordion">
+        <div className={selected === index ? "accordion show" : "accordion"}>
           <div className="accordion-title">
             {title}
-            <span className="arrow-down"></span>
+            <span
+              className={selected === index ? "arrow-down" : "arrow-up"}
+              onClick={() => toggle(index)}
+            ></span>
           </div>
-          <div className="accordion-content">{accordionContent[index]}</div>
+          <div className={selected === index ? "wrapper is-open" : "wrapper"}>
+            <div
+              className={
+                selected === index
+                  ? "accordion-content show"
+                  : "accordion-content hide"
+              }
+            >
+              {accordionContent[index]}
+            </div>
+          </div>
         </div>
       ))}
     </div>
