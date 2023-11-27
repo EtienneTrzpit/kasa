aaf7a09cc848d97c39e3410cbd2a8083defb4cdd
Etienne Trzpit <etienne.trzpit@free.fr>
2023-11-27 12:30:30 +0100

    transition accordion

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,11 +1,28 @@
 import "../assets/style/accordion.scss";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
+import { useLocation } from "react-router-dom";
 
 export default function Accordion({ accordionTitle, accordionContent }) {
   const [selected, setSelected] = useState(null);
   const toggle = () => {
     selected === null ? setSelected(0) : setSelected(null);
   };
+  const accordion = useRef();
+  const location = useLocation();
+  useEffect(() => {
+    if (
+      selected === 0 &&
+      location.pathname.startsWith("/location") &&
+      window.innerWidth > 768
+    ) {
+      accordion.current.style.height = `200px`;
+      console.log("true");
+    } else if (selected === 0) {
+      accordion.current.style.height = `${accordion.current.scrollHeight}px`;
+    } else {
+      accordion.current.style.height = "0px";
+    }
+  }, [selected]);
   return (
     <div
       className={selected === 0 ? "accordion active" : "accordion"}
@@ -20,7 +37,9 @@ export default function Accordion({ accordionTitle, accordionContent }) {
           onClick={() => toggle()}
         ></span>
       </div>
-      <div className="accordion-content">{accordionContent}</div>
+      <div className="accordion-content" ref={accordion}>
+        {accordionContent}
+      </div>
     </div>
   );
 }
